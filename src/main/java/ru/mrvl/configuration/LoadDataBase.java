4d5a461f4bf24577dd8d336fab964850a3b013be
MaksimchikPerchikov <maksim.perch999@mail.ru>
2022-01-11 17:24:37 +0300

    много нового

---
+++ src/main/java/ru/mrvl/configuration/LoadDataBase.java
@@ -0,0 +1,2 @@
+package ru.mrvl.configuration;public class LoadDataBase {
+}
--- src/main/java/ru/mrvl/repository/GeneralizationRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mrvl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.mrvl.model.Generalization;
-
-@Repository
-public interface GeneralizationRepository extends JpaRepository<Generalization,Long> {
-}
